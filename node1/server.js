c692942a6a6412eeb24b005d4ec52abb98b19f2f
Sudeep Mishra <sudeepm0599@gmail.com>
2024-10-23 19:05:46 +0545

    changes

---
+++ node1/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node 1');
+});
+
+app.get('/node2', (req, res) => {
+  res.redirect('http://ming-node2:3000');
+});
+
+app.get('/node3', (req, res) => {
+  res.redirect('http://ming-node3:3000');
+});
+
+app.listen(port, () => {
+  console.log(`Node 1 server running at http://localhost:${port}`);
+});
